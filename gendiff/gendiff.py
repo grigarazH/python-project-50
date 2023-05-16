111077c4f6019eeb72e2c9d85dfff473815ae471
grigarazh <grigarazh@yandex.ru>
2023-05-16 21:28:08 +0300

    refactor: added get_value_diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,6 +5,20 @@ def format_bool(value):
     return str(value).lower() if isinstance(value, bool) else value
 
 
+def get_value_diff(key, value1, value2):
+    value1_formatted = format_bool(value1)
+    value2_formatted = format_bool(value2)
+    result = ""
+    if value1 != value2:
+        if value1 is not None:
+            result += f"  - {key}: {value1_formatted}\n"
+        if value2 is not None:
+            result += f" + {key}: {value2_formatted}\n"
+    else:
+        result += f"    {key}: {value1_formatted}\n"
+    return result
+
+
 def generate_diff(file_path1, file_path2):
     file1 = json.load(open(file_path1))
     file2 = json.load(open(file_path2))
@@ -13,14 +27,6 @@ def generate_diff(file_path1, file_path2):
         file_diff[key] = file1.get(key), value
     result = "{\n"
     for key, value in sorted(file_diff.items()):
-        value1_formatted = format_bool(value[0])
-        value2_formatted = format_bool(value[1])
-        if value[0] != value[1]:
-            if value[0] is not None:
-                result += f"  - {key}: {value1_formatted}\n"
-            if value[1] is not None:
-                result += f"  + {key}: {value2_formatted}\n"
-        else:
-            result += f"    {key}: {value1_formatted}\n"
+        result += get_value_diff(key, value[0], value[1])
     result += "}"
     return result
